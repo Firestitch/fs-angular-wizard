828a30af1003c86f6299ce8bfa368e8532c16ad0
Raymond <ray@firestitch.com>
2017-06-16 12:07:25 -0400

    Initial commit

---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-wizard',[])
+    .directive('fsWizard', function() {
+        return {
+            templateUrl: 'views/directives/wizard.html',
+            restrict: 'E',
+            scope: {
+               
+            },
+            controller: function($scope) {
+            
+            },
+            link: function($scope, element, attrs) {
+               
+            }
+        };
+    });
+})();
